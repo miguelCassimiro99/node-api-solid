e93c76fba3cde828b4ace26d10c98d87413937aa
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2022-02-16 20:36:04 -0300

    added repository to test

---
+++ tests/repositories/in-memory-challenges-repository.ts
@@ -0,0 +1,18 @@
+import {ChallengesRepository} from "../../src/application/repositories/ChallengesRepository";
+import {Challenge} from "../../src/domain/entitites/challenge";
+
+
+export class InMemoryChallengesRepository implements ChallengesRepository {
+    public items: Challenge[] = [];
+
+
+    async findById(id: string): Promise<Challenge | null> {
+        const challenge = this.items.find(challenge => challenge.id === id);
+
+        if(!challenge) {
+            return null;
+        }
+
+        return challenge;
+    }
+}
